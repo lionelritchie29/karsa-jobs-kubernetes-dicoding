76cd3e39587c8fa98108be9320a5363cb22c669d
lionelritchie29 <lionelrtchieee@gmail.com>
2022-11-29 10:39:06 +0700

    build: add script for building and pushing docker images

---
+++ karsajobs-backend/build_push_image_karsajobs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#build docker image from Dockerfile from current directory with name lionelritchie29/karsajobs and tag latest
+docker build -t lionelritchie29/karsajobs:latest .
+echo '\n\n' #print new line to give some space
+
+# create a new image tag from source image, still refer to the same image, but have different name/tag
+docker tag lionelritchie29/karsajobs:latest ghcr.io/lionelritchie29/karsajobs:latest
+echo '\n\n' #print new line to give some space
+
+# export github token to env variable
+export GITHUB_PACKAGES_TOKEN=
+
+# login to github packages
+echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u lionelritchie29 --password-stdin
+echo '\n\n' #print new line to give some space
+
+# push the built image to github packages
+docker push ghcr.io/lionelritchie29/karsajobs:latest
+echo '\n\n' #print new line to give some space
